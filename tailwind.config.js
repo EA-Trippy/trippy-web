d2303bc6f74013dacbc8a89a3bfabd21461a84e4
subsub-e <ab13696802@gmail.com>
2023-07-16 14:34:08 +0900

    [TRP-49] HEADER

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      color:{
+        white: "#ffffff",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
