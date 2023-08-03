000868644c05c72097ae7a42d4f8f8236c39bd85
subsub-e <ab13696802@gmail.com>
2023-08-03 22:28:37 +0900

    [TRP-56] 글쓰기 페이지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "react-quill/dist/quill.snow.css";
+import "@/styles/textEditor.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
